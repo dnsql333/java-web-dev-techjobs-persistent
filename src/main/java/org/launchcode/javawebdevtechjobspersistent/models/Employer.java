249852a8f4e639f352041e04fbdd8692e089ce64
Taeeun <Kimtaeeun92@gmail.com>
2021-03-15 20:37:04 -0500

    part 3

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank
     private String location;
+
+
+    // 3.1
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
 
     public String getLocation() {
         return location;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -3,41 +3,31 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.*;
 
 @Entity
-public class Job{
+public class Job extends AbstractEntity{
 
-    @Id
-    @GeneratedValue
-    private int id;
-
-    private String name;
-
-    private String employer;
+//    private String employer;
     private String skills;
+
+    //3.2
+    @ManyToOne
+    private Employer employer;
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+    //3.2 switched to "Employer anEmployer" from "String anEmployer"
+    public Job(Employer anEmployer, String someSkills) {
         super();
         this.employer = anEmployer;
         this.skills = someSkills;
     }
 
-    // Getters and setters.
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
@@ -48,4 +38,24 @@ public class Job{
     public void setSkills(String skills) {
         this.skills = skills;
     }
-}
+
+    }
+
+
+
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//
+//    private String name;
+
+
+// Getters and setters.
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
