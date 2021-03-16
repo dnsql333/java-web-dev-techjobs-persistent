3fd8f6ed4b0b9fbfd1c8bd53cefb5bdce2506051
Taeeun <Kimtaeeun92@gmail.com>
2021-03-15 19:29:08 -0500

    completed part 2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,15 @@
 ## Part 1: Test it with SQL
 
+id int PK
+employer varchar(255)
+name varchar(255)
+skills varchar(255)
+
 ## Part 2: Test it with SQL
+
+SELECT *
+FROM employer
+WHERE location = "St.Louis"
 
 ## Part 3: Test it with SQL
 
